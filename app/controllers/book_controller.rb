cc7351ee454aae254460fd33f115a58267176dd5
shoheikitabatake <batake0919@gmail.com>
2015-10-23 10:45:14 +0000

    [feature]残高 ver2.0
    + groupごとにアコーディオン表示
    + chart.jsの活用
    
    + スマホサイズに対応できていない
    + ログイン後初読み込み時に

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,7 +1,7 @@
 class BookController < ApplicationController
   def balance_sheet
-    @properties = Property.where( user_id: current_user.id )
-    @debts = Debt.where( user_id: current_user.id )
+    @property_groups = PropertyGroup.where( user_id: current_user.id )
+    @debt_groups = DebtGroup.where( user_id: current_user.id )
   end
 
   def profit_loss_statement
--- app/helpers/book_helper.rb
+++ app/helpers/book_helper.rb
@@ -1,2 +1,43 @@
 module BookHelper
+  # color, hightlightのパターンを取得
+  # @params [integer] order 何番目か
+  # @return [array]
+  def getColorPattern ( order )
+    patterns = {
+      1 => {# 赤
+        "color" => "#C7243A",
+        "highlight" => "#ECACB5",
+      },
+      2 => {# 黄色
+        "color" => "#F3C759",
+        "highlight" => "#F9E3AA",
+      },
+      3 => {# 黄緑
+        "color" => "#A4C520",
+        "highlight" => "#DFECAA",
+      },
+      4 => {# 緑
+        "color" => "#009250",
+        "highlight" => "#91DBB9",
+      },
+      5 => {# 青
+        "color" => "#0086AB",
+        "highlight" => "#97D3E3",
+      },
+      6 => {# 紺
+        "color" => "#3261AB",
+        "highlight" => "#D5E0F1",
+      },
+      7 => {# 紫
+        "color" => "#5D639E",
+        "highlight" => "#BFC2DF",
+      },
+    }
+
+    if patterns.has_key?( order )
+      return patterns[order]
+    else
+      return patterns[patterns.length]
+    end
+  end
 end
